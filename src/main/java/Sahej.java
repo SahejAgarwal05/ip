ba0617e2090a1fe3eb8ed0d642ff2e9dbe0b989a
Sahej Agarwal <sahej.a05@gmail.com>
2025-01-24 15:50:58 +0800

    Changed Assistant Name to Sahej

---
+++ src/main/java/Sahej.java
@@ -0,0 +1,7 @@
+public class Sahej {
+    public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________\n";
+        System.out.println(horizontalLine+" Hello! I'm Sahej\n What can I do for you?\n"+horizontalLine+" Bye. Hope to see you again soon!\n"+horizontalLine);
+    }
+}
+
