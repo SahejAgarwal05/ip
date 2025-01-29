dfb1122c6fac941d23d90b2d253ca927690ce8ca
Sahej Agarwal <sahej.a05@gmail.com>
2025-01-29 15:03:23 +0800

    Echo feature completed

--- src/main/java/Sahej.java
+++ src/main/java/Sahej.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
 public class Sahej {
     public static void main(String[] args) {
-        String horizontalLine = "____________________________________________________________\n";
-        System.out.println(horizontalLine+" Hello! I'm Sahej\n What can I do for you?\n"+horizontalLine+" Bye. Hope to see you again soon!\n"+horizontalLine);
+        String horizontalLine = "\t____________________________________________________________";
+        System.out.println(horizontalLine+"\n\tHello! I'm Sahej\n\tWhat can I do for you?\n"+horizontalLine);
+        Scanner userInput = new Scanner(System.in);
+        String input = "";
+        while(true){
+            input = userInput.nextLine();
+            System.out.println(horizontalLine);
+            if (input.equals("bye")){
+                break;
+            }
+            System.out.println("\t" + input);
+            System.out.println(horizontalLine + "\n");
+        }
+        System.out.println("\tBye. Hope to see you again soon!\n" + horizontalLine);
     }
 }
 
