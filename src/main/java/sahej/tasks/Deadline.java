aaf415727b4c16b025cf00dbad1e35c8ab8bc779
Sahej Agarwal <sahej.a05@gmail.com>
2025-03-01 15:40:06 +0800

    added proper dat

--- src/main/java/sahej/tasks/Deadline.java
+++ src/main/java/sahej/tasks/Deadline.java
@@ -1,21 +1,30 @@
 package sahej.tasks;
 import sahej.ui.*;
+import sahej.ui.ErrorExceptions;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends ToDo {
-    private String by;
+    private LocalDate by;
 
     public Deadline (String name, String by) throws SahejException {
         if (by == null || by.equals("") || name == null || name.equals("")) {
             throw ErrorExceptions.INVALID_DEADLINE_INPUT;
         }
+        try {
+            this.by = LocalDate.parse(by);
+        } catch (Exception e) {
+            throw ErrorExceptions.INVALID_DEADLINE_DATE.;
+        }
         this.name = name;
-        this.by = by;
     }
 
     /**
      * returns the time of the deadline
      * @return
      */
-    public String getBy() {
+    public LocalDate getBy() {
         return by;
     }
 
@@ -23,16 +32,16 @@ public class Deadline extends ToDo {
      *alter the deadline time
      * @param by
      */
-    public void setBy(String by) {
+    public void setBy(LocalDate by) {
         this.by = by;
     }
 
     @Override
     public String toString() {
         if (this.isCompleted) {
-            return "[X][D] " + this.getName() + " (by: " + this.by + ")";
+            return "[X][D] " + this.getName() + " (by: " + this.by.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + ")";
         } else {
-            return "[ ][D] " + this.getName() + " (by: " + this.by + ")";
+            return "[ ][D] " + this.getName() + " (by: " + this.by.format(DateTimeFormatter.ofPattern("MMM dd yyyy"))  + ")";
         }
     }
     /**
@@ -48,7 +57,7 @@ public class Deadline extends ToDo {
             saveString += " |";
         }
         saveString += this.name + "|";
-        saveString += this.by;
+        saveString += this.by.toString();
         return saveString + "\n";
     }
 }
